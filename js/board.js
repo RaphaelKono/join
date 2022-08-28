c354e2f734f9645cad7c5be3806d871a90f3686a
RaphaelKono <raphaelkonopatzki@web.de>
2022-08-28 19:03:17 +0200

    added layout of board

---
+++ js/board.js
@@ -0,0 +1,21 @@
+let boardTasks = [{
+    'title': 'Prepare presentation',
+    'id': 1,
+    'urgency': 'Medium',
+    'category': 'Marketing',
+    'dueDate': 'XX.XX.XXXX',
+    'currentDate': 'ZZ.ZZ.ZZZZ',
+    'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, inventore possimus accusamus rem repudiandae numquam hic accusantium ratione corrupti, iste laborum facilis voluptas praesentium nobis omnis provident itaque ea quis.',
+    'assignedTo': 'Mark Wahlberg',
+    'status': 'ToDo'
+}, {
+    'title': 'Present presentation',
+    'id': 2,
+    'urgency': 'Medium',
+    'category': 'Managment',
+    'dueDate': 'XX.XX.XXXX',
+    'currentDate': 'ZZ.ZZ.ZZZZ',
+    'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, inventore possimus accusamus rem repudiandae numquam hic accusantium ratione corrupti, iste laborum facilis voluptas praesentium nobis omnis provident itaque ea quis.',
+    'assignedTo': 'CEO',
+    'status': 'ToDo'
+}];
