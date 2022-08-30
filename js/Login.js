9d6e8a143ebb3ee0e0b58d63875f64ecff641138
thorben möller <106166747+Thorbenmoe@users.noreply.github.com>
2022-08-30 19:52:15 +0200

    Update Login.js

--- js/Login.js
+++ js/Login.js
@@ -1,7 +1,23 @@
 const loginForm = document.querySelector("#login");
 const createAccountForm = document.querySelector("#regrister");
 
-
+var user = [{
+        username: "user1",
+        password: "123456"
+    },
+    {
+        username: "user2",
+        password: "123456"
+    },
+    {
+        username: "user3",
+        password: "123456"
+    },
+    {
+        username: "user4",
+        password: "123456"
+    },
+]
 
 
 
@@ -15,4 +31,18 @@ function createAcc() {
 
     loginForm.classList.add("form_hidden");
     createAccountForm.classList.remove("form_hidden");
+}
+
+function login() {
+    var username = document.getElementById('loginUserName').value
+    var password = document.getElementById('loginPassword').value
+    for (let i = 0; i < user.length; i++) {
+        if (username == user[i].username && password == user[i].password) {
+            console.log(username + " is logged in!!!")
+
+            return
+        } else {
+            console.log("username or password is wrong")
+        }
+    }
 }
