1023bce0422b4aab1a0c81a0fe8a399a4dba37ab
Neufeld <vn@rodenbergmail.de>
2022-08-31 06:24:11 +0200

    #

--- js/login.js
+++ js/login.js
@@ -1,50 +1,48 @@
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form_message");
+const loginForm = document.querySelector("#login");
+const createAccountForm = document.querySelector("#regrister");
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form_message_success", "form_message_error");
-    messageElement.classList.add(`form_message_${type}`);
+var user = [{
+        username: "user1",
+        password: "123456"
+    },
+    {
+        username: "user2",
+        password: "123456"
+    },
+    {
+        username: "user3",
+        password: "123456"
+    },
+    {
+        username: "user4",
+        password: "123456"
+    },
+]
 
+
+
+function sumbit() {
+
+    loginForm.classList.remove("form_hidden");
+    createAccountForm.classList.add("form_hidden");
 }
 
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form_input_error");
+function createAcc() {
+
+    loginForm.classList.add("form_hidden");
+    createAccountForm.classList.remove("form_hidden");
 }
 
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form_input_error");
-    inputElement.parentElement.querySelector("orm_input_error_message").textContent = "error."
+function login() {
+    var username = document.getElementById('loginUserName').value
+    var password = document.getElementById('loginPassword').value
+    for (let i = 0; i < user.length; i++) {
+        if (username == user[i].username && password == user[i].password) {
+            console.log(username + " is logged in!!!")
+
+            return
+        } else {
+            console.log("username or password is wrong")
+        }
+    }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#createAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form_hidden");
-        createAccountForm.classList.remove("form_hidden");
-    });
-
-    document.querySelector("#linklogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form_hidden");
-        createAccountForm.classList.add("form_hidden");
-    });
-    loginForm.addEventListener("sumbit", e => {
-        e.preventDefault();
-        setFormMessage(loginForm, "error", "Ungültiger Benutzername oder das Passwort ist falsch.")
-    });
-
-    document.querySelectorAll(".form_input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "der Benutzername darf nicht länger als 10 Zeichen sein!");
-            }
-
-        });
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
