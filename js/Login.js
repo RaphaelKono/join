761d25b4baecd1282c3097067bf60613963e6ba9
Valer Neufeld <valerneufeld@outlook.de>
2022-09-17 11:02:25 +0200

    #

--- js/Login.js
+++ js/Login.js
@@ -35,8 +35,8 @@ async function login() {
 function changeColor() {
     document.getElementById("loginUserName").style.color = "red";
     document.getElementById("loginPassword").style.color = "red";
-     document.getElementsById("loginUserName").style.borderColor = "red"
-    document.getElementsById("loginPassword").style.borderColor = "red"
+    document.getElementById("loginUserName").style.borderColor = "red"
+    document.getElementById("loginPassword").style.borderColor = "red"
     document.getElementById("error").innerHTML = "password or username is wrong.";
 }
 
