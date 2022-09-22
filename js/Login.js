aaf432a62366f7fcc0f60c61e418c3d475d5e7ee
RaphaelKono <raphaelkonopatzki@web.de>
2022-09-22 16:21:18 +0200

    added cleancode to login

--- js/Login.js
+++ js/Login.js
@@ -7,119 +7,129 @@ let loginUsername = document.getElementById("log-username");
 let loginPassword = document.getElementById("log-password");
 
 async function initLogin() {
-  setURL("https://gruppe-302.developerakademie.net/smallest_backend_ever");
-  await downloadFromServer();
-  await getUsersFromServer();
+    setURL("https://gruppe-302.developerakademie.net/smallest_backend_ever");
+    await downloadFromServer();
+    await getUsersFromServer();
 }
 
 function sumbit() {
-  loginForm.classList.remove("form_hidden");
-  createAccountForm.classList.add("form_hidden");
+    loginForm.classList.remove("form_hidden");
+    createAccountForm.classList.add("form_hidden");
 }
 
 function createAcc() {
-  loginForm.classList.add("form_hidden");
-  createAccountForm.classList.remove("form_hidden");
+    loginForm.classList.add("form_hidden");
+    createAccountForm.classList.remove("form_hidden");
 }
 
 loginForm.addEventListener(
-  "submit",
-  function (event) {
-    checkUsername();
-    checkPassword();
-    if (!loginForm.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    loginForm.classList.add("was-validated");
-  },
-  false
+    "submit",
+    function(event) {
+        checkUsername();
+        checkPassword();
+        if (!loginForm.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        loginForm.classList.add("was-validated");
+    },
+    false
 );
 
 function checkUsername() {
-  for (let i = 0; i < backendUsers.length; i++) {
-    if (document.getElementById("loginUserName").value == backendUsers[i].userName) {
-      loginUsername.removeAttribute("required");
-      return;
+    for (let i = 0; i < backendUsers.length; i++) {
+        if (document.getElementById("loginUserName").value == backendUsers[i].userName) {
+            loginUsername.removeAttribute("required");
+            return;
+        }
     }
-  }
-  loginUsername.setAttribute("required", "");
+    loginUsername.setAttribute("required", "");
 }
 
 function checkPassword() {
-  for (let i = 0; i < backendUsers.length; i++) {
-    if (document.getElementById("loginPassword").value == backendUsers[i].password) {
-      loginPassword.removeAttribute("required");
-      return;
+    for (let i = 0; i < backendUsers.length; i++) {
+        if (document.getElementById("loginPassword").value == backendUsers[i].password) {
+            loginPassword.removeAttribute("required");
+            return;
+        }
     }
-  }
-  loginPassword.setAttribute("required", "");
+    loginPassword.setAttribute("required", "");
 }
 
 async function login() {
-  var username = document.getElementById("loginUserName").value;
-  var password = document.getElementById("loginPassword").value;
-  for (let i = 0; i < backendUsers.length; i++) {
-    if (username == backendUsers[i].userName && password == backendUsers[i].password) {
-      await backend.setItem("currentUser", JSON.stringify(backendUsers[i].id));
-      return (location.href = "html/addTask.html");
+    var username = document.getElementById("loginUserName").value;
+    var password = document.getElementById("loginPassword").value;
+    for (let i = 0; i < backendUsers.length; i++) {
+        if (username == backendUsers[i].userName && password == backendUsers[i].password) {
+            await backend.setItem("currentUser", JSON.stringify(backendUsers[i].id));
+            return (location.href = "html/addTask.html");
+        }
     }
-  }
 }
 
 createAccountForm.addEventListener(
-  "submit",
-  function (event) {
-    if (!createAccountForm.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    createAccountForm.classList.add("was-validated");
-  },
-  false
+    "submit",
+    function(event) {
+        if (!createAccountForm.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        createAccountForm.classList.add("was-validated");
+    },
+    false
 );
 
 async function registerUsers() {
-  if (createAccountForm.checkValidity()) {
-    setNewUser();
-    await backendUsers.push(newUser);
-    await backend.setItem("users", JSON.stringify(backendUsers));
-    await backend.setItem("currentUser", JSON.stringify(newUser.id));
-    location.href = "html/addTask.html";
-  }
+    if (createAccountForm.checkValidity()) {
+        setNewUser();
+        await backendUsers.push(newUser);
+        await backend.setItem("users", JSON.stringify(backendUsers));
+        await backend.setItem("currentUser", JSON.stringify(newUser.id));
+        location.href = "html/addTask.html";
+    }
 }
 
 function setNewUser() {
-  newUser = {
-    id: backendUsers.length + 1,
-    userName: document.getElementById("signupUsername").value,
-    password: document.getElementById("signUpPassword").value,
-    firstName: document.getElementById("signupfirstname").value,
-    lastName: document.getElementById("signuplastname").value,
-    email: document.getElementById("userEmail").value,
-    src: "../img/" + document.getElementById(selectedNewUser).src.split("/img/")[1],
-  };
+    newUser = {
+        id: backendUsers.length + 1,
+        userName: document.getElementById("signupUsername").value,
+        password: document.getElementById("signUpPassword").value,
+        firstName: document.getElementById("signupfirstname").value,
+        lastName: document.getElementById("signuplastname").value,
+        email: document.getElementById("userEmail").value,
+        src: "../img/" + document.getElementById(selectedNewUser).src.split("/img/")[1],
+    };
 }
 
 function selectNewUser(i) {
-  let user = document.getElementById("userNew-" + i);
-  if (i != 1) user.classList.toggle("avatar-selected");
-  if (selectedNewUser.includes("userNew-" + i)) {
+    let user = document.getElementById("userNew-" + i);
+    if (i != 1) user.classList.toggle("avatar-selected");
+    if (isSelectedUserDeselected(i))
+        setDefaultUser();
+    else
+        setSelectedUser(i);
+}
+
+function isSelectedUserDeselected(i) {
+    return selectedNewUser.includes("userNew-" + i);
+}
+
+function setDefaultUser() {
     selectedNewUser = "userNew-1";
     document.getElementById("userNew-1").classList.add("avatar-selected");
-  } else {
+}
+
+function setSelectedUser(i) {
     selectedNewUser = "userNew-" + i;
-    if (i == 1) {
-      document.getElementById("userNew-1").classList.add("avatar-selected");
-    }
+    if (i == 1)
+        document.getElementById("userNew-1").classList.add("avatar-selected");
     for (let j = 1; j < 4; j++) {
-      if (j == i) continue;
-      document.getElementById("userNew-" + j).classList.remove("avatar-selected");
+        if (j == i) continue;
+        document.getElementById("userNew-" + j).classList.remove("avatar-selected");
     }
-  }
 }
 
 async function loginAsGuest() {
-  await backend.setItem("currentUser", JSON.stringify(5));
-  return (location.href = "html/addTask.html");
+    await backend.setItem("currentUser", JSON.stringify(5));
+    return (location.href = "html/addTask.html");
 }
